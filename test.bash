64c8e34210de5f30c967c96eb185eb17f91e2378
Taiki Nakazawa <nakazawa15623@gmail.com>
2025-10-30 13:31:28 +0900

    add debug

--- test.bash
+++ test.bash
@@ -7,16 +7,18 @@ ng () {
 }
 
 res=0
-
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out 15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo 0 | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
 exit $res
-+ exit 0
