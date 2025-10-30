0ce37b5c35100f23a3f28bc768f09a16963e2d8f
Taiki Nakazawa <nakazawa15623@gmail.com>
2025-10-30 12:28:06 +0900

    change name plus_stdin to plus

---
+++ test.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Taiki Nakazawa
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+       echo ${1}行目が違うよ
+       res=1
+}
+
+res=0
+a=山田
+[ "$a" = 上田 ] || ng "$LINENO"
+[ "$a" = 山田 ] || ng "$LINENO"
+
+exit $res
