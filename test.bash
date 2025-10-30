9f2beaab8f3a5a5d8266bb28c21a90b69d516e1d
Taiki Nakazawa <nakazawa15623@gmail.com>
2025-10-30 13:20:10 +0900

    add debug

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Taiki Nakazawa
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
@@ -9,7 +9,14 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out 15
 [ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
 
 [ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
 exit $res
++ exit 0
