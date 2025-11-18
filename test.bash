5f48aa4417dcff14f949edc9feb0261630aea5c3
Taiki Nakazawa <nakazawa15623@gmail.com>
2025-11-18 14:26:24 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -1,22 +1,56 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2025 Taiki Nakazawa
-# SPDX-License-Identifier: BSD-3-Clause
-ng () {
-       echo ${1}行目が違うよ
-       res=1
+
+# テスト対象プログラム
+CMD="./kadai1"
+# エラー時にどの行が失敗したか表示する関数
+ng() {
+    echo "NG: line $1: expected '$2' but got '$3'"
+    exit 1
 }
+# OK なら表示
+ok() {
+    echo "OK: $1"
+}
+### TEST 1: シンプルな英数字混合 ###
+input="B3a1!!c"
+expected="13abc"
+output=$(echo "$input" | $CMD)
+[ "$output" = "$expected" ] || ng $LINENO "$expected" "$output"
+ok "simple input"
 
-res=0
-### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
-[ "${res}" = 0 ] && echo OK
-+ echo OK
-OK
-exit $res
-+ exit 0
+### TEST 2: 大文字小文字混在 ###
+input="Hello123 ABC"
+expected="123abcehllo"
+output=$(echo "$input" | $CMD)
+[ "$output" = "$expected" ] || ng $LINENO "$expected" "$output"
+ok "case normalize"
+
+
+### TEST 3: 記号のみ ###
+input="!!!!???--"
+expected=""
+output=$(echo "$input" | $CMD)
+[ "$output" = "$expected" ] || ng $LINENO "$expected" "$output"
+ok "symbol only"
+
+
+### TEST 4: 空行 ###
+input=""
+expected=""
+output=$(echo "$input" | $CMD)
+[ "$output" = "$expected" ] || ng $LINENO "$expected" "$output"
+ok "empty line"
+
+
+### TEST 5: 複数行入力 ###
+input=$'A3b\nz9!?X'
+expected=$'3ab\n9xz'
+output=$(echo "$input" | $CMD)
+[ "$output" = "$expected" ] || ng $LINENO "$expected" "$output"
+ok "multi-line input"
+
+
+echo "==== ALL TESTS PASSED ===="
+exit 0
+
