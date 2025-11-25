f5dc100cece6a1540ef0dcfbcca44bca4d730ef7
Taiki Nakazawa <nakazawa15623@gmail.com>
2025-11-25 13:56:30 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,17 +1,16 @@
 #!/bin/bash -xv
 
-# テスト対象プログラム
 CMD="./kadai1"
-# エラー時にどの行が失敗したか表示する関数
+
 ng() {
     echo "NG: line $1: expected '$2' but got '$3'"
     exit 1
 }
-# OK なら表示
+
 ok() {
     echo "OK: $1"
 }
-### TEST 1: シンプルな英数字混合 ###
+
 input="B3a1!!c"
 expected="13abc"
 output=$(echo "$input" | $CMD)
@@ -19,7 +18,7 @@ output=$(echo "$input" | $CMD)
 ok "simple input"
 
 
-### TEST 2: 大文字小文字混在 ###
+
 input="Hello123 ABC"
 expected="123abcehllo"
 output=$(echo "$input" | $CMD)
@@ -27,7 +26,7 @@ output=$(echo "$input" | $CMD)
 ok "case normalize"
 
 
-### TEST 3: 記号のみ ###
+
 input="!!!!???--"
 expected=""
 output=$(echo "$input" | $CMD)
@@ -35,7 +34,7 @@ output=$(echo "$input" | $CMD)
 ok "symbol only"
 
 
-### TEST 4: 空行 ###
+
 input=""
 expected=""
 output=$(echo "$input" | $CMD)
@@ -43,7 +42,7 @@ output=$(echo "$input" | $CMD)
 ok "empty line"
 
 
-### TEST 5: 複数行入力 ###
+
 input=$'A3b\nz9!?X'
 expected=$'3ab\n9xz'
 output=$(echo "$input" | $CMD)
